5d12ef79acd1736f6da25b666bda4552d652be89
Denis Krienbühl <denis@href.ch>
2014-10-27 15:59:56 +0100

    Automatically append /@@kml-document to the urls that can't be found

--- seantis/placemap/utils.py
+++ seantis/placemap/utils.py
@@ -1,9 +1,23 @@
+import fastkml
+import lxml
 import requests
 
 
 def fetch_kml_document(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        return None
+
+    candidates = (url, url + '/@@kml-document')
+
+    for url in candidates:
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            continue
+
+        try:
+            fastkml.kml.KML().from_string(response.content)
+        except (lxml.etree.XMLSyntaxError, TypeError):
+            continue
+        else:
+            return response.content
+
+    return None
